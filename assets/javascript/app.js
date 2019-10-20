b440255828fd47eb1b2c97966a4da37699e9d1ce
Sarah Naas <sjnaas@gmail.com>
2019-10-19 20:27:15 -0700

    fixed grading system and added css

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,5 +1,5 @@
 var intervalId;
-var twoMinutes = 121;
+var twoMinutes = 120;
 var correct = 0;
 var incorrect = 0;
 var unanswered = 0;
@@ -9,31 +9,38 @@ var myQuiz = [
     question: "What was Emperor Norton's first name?",
     name: "first-name",
     answers: ["John", "Joshua", "Jeremiah", "James"],
-    correctAnswer: 2
+    correctAnswer: 1
   },
   {
     question: "What year was Emperor Norton Born?",
     name: "age",
     answers: ["1819", "1805", "1823", "1811"],
-    correctAnswer: 1
+    correctAnswer: 0
   },
   {
-    question: "What year was Emperor Norton Born?",
-    name: "Ho",
-    answers: ["1819", "1805", "1823", "1811"],
+    question: "What did Emperor Norton declare himself the protector of?",
+    name: "protector",
+    answers: ["Whales", "The people", "Mexico", "Nature"],
+    correctAnswer: 2
+  },
+  {
+    question: "How did Emperor Norton lose his fortune?",
+    name: "fortune",
+    answers: ["Kale", "Potatoes", "Corn", "Rice"],
+    correctAnswer: 3
+  },
+  {
+    question: "What year did he declare himself Emperor?",
+    name: "year",
+    answers: ["1856", "1859", "1861", "1864"],
     correctAnswer: 1
   }
 ];
-
-//-------------Start Button code
-//Get into the habit of using $(document).ready(function() {---})
 
 $(document).ready(function() {
   $("#btnStart").on("click", game);
   $("#btnDone").on("click", stopTimer);
 });
-
-//-----------Timer code.
 
 function countdown() {
   twoMinutes--;
@@ -42,9 +49,6 @@ function countdown() {
     stopTimer();
   }
 }
-
-//MISSING: When timer hits 0 stop the game and show the results
-//--------Game begins.
 
 function game() {
   if (!timerRunning) {
@@ -57,14 +61,10 @@ function game() {
 function stopTimer() {
   clearInterval(intervalId);
   timerRunning = false;
-  // checkForm();
   changeScreen2();
   gradeForm();
 }
 
-// is this the best way to turn multiple things invisible?
-//or should I put the code into each function where this is
-// called individually?
 function changeScreen1() {
   $("#btnStart")
     .removeClass("show")
@@ -83,7 +83,7 @@ function changeScreen2() {
     .addClass("show");
 }
 
-//------code to grade the game.
+
 
 play();
 
@@ -92,57 +92,46 @@ function play() {
     $(`
     <h3>${myQuiz[i].question}</h3>`).appendTo(".quiz-content");
     $(
-      `<input type="radio" name=${myQuiz[i].name} value="1">${
+      `<input type="radio" name=${myQuiz[i].name} value="0" >${
         myQuiz[i].answers[0]
       }</input>`
     ).appendTo(".quiz-content");
     $(
-      `<input type="radio" name=${myQuiz[i].name} value="2">${
+      `<input type="radio" name=${myQuiz[i].name} value="1" >${
         myQuiz[i].answers[1]
       }</input>`
     ).appendTo(".quiz-content");
     $(
-      `<input type="radio" name=${myQuiz[i].name} value="3">${
+      `<input type="radio" name=${myQuiz[i].name} value="2" >${
         myQuiz[i].answers[2]
       }</input>`
     ).appendTo(".quiz-content");
     $(
-      `<input type="radio" name=${myQuiz[i].name} value="4">${
+      `<input type="radio" name="${myQuiz[i].name}" value="3" >${
         myQuiz[i].answers[3]
       }</input>`
     ).appendTo(".quiz-content");
   }
 }
 
-//it knows that something has been selected, but will only check for the first question
-// function gradeForm() {
-//   for (var i = 0; i < myQuiz.length; i++) {
-//     if ($("input:radio[name]").is(":checked")) {
-//       console.log("yo");
-//     } else {
-//       unanswered++;
-//       $(".unanswered-qs").html(`<h3>Unanswered Questions: ${unanswered} </h3>`);
-//     }
-//   }
-// }
-
 function gradeForm() {
-  console.log("I");
-}
 
-// function gradeForm() {
-//   var userGuess = $("input[value]:checked").val();
-//   console.log(userGuess);
-//   for (var i = 0; i < myQuiz.length; i++) {
-//     if (userGuess == myQuiz.correctAnswer) {
-//       correct++;
-//       console.log("heeeeey ya");
-//     } else if (userGuess !== myQuiz.correctAnswer) {
-//       incorrect++;
-//       $(".incorrect-guesses").html(`<h3>Incorrect Guesses: ${incorrect} </h3>`);
-//     } else {
-//       unanswered++;
-//       $(".unanswered-qs").html(`<h3>Unanswered Questions: ${unanswered} </h3>`);
-//     }
-//   }
-// }
+  for (var i = 0; i < myQuiz.length; i++) {
+    var userGuess = $("input[name='" + myQuiz[i].name + "']:checked").val();
+
+    if (userGuess) {
+      if (userGuess == myQuiz[i].correctAnswer) {
+        correct++;
+        $(".correct-guesses").html(`<h3>Correct Guesses: ${correct} </h3>`);
+      } else {
+        incorrect++;
+        $(".incorrect-guesses").html(
+          `<h3>Incorrect Guesses: ${incorrect} </h3>`
+        );
+      }
+    } else {
+      unanswered++;
+      $(".unanswered-qs").html(`<h3>Unanswered Questions: ${unanswered} </h3>`);
+    }
+  }
+
